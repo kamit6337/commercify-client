f04a77325778dfb706694d43d0772103a753b143
Kamit6337 <kamit6337@gmail.com>
2024-05-27 14:35:25 +0530

    update payment failed

--- src/pages/payment/PaymentCancel.jsx
+++ src/pages/payment/PaymentCancel.jsx
@@ -6,35 +6,10 @@ import { useDispatch } from "react-redux";
 import { failedOrders } from "../../redux/slice/userOrdersSlice";
 
 const PaymentCancel = () => {
-  const dispatch = useDispatch();
-  const { isLoading, error, data } = useFailedBuyProducts();
-
-  useEffect(() => {
-    if (data) {
-      dispatch(failedOrders(data.data));
-    }
-  }, [data, dispatch]);
-
-  if (isLoading) {
-    return (
-      <div className="h-96 w-full">
-        <Loading />
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="h-96 w-full flex justify-center items-center">
-        <p>{error.message}</p>
-      </div>
-    );
-  }
-
   return (
     <section className="bg-gray-100 p-5">
       <main className="bg-white p-5">
-        <p className="text-2xl p-5 text-red-500 ">{data.message}</p>
+        <p className="text-2xl p-5 text-red-500 ">Your Payment is Failed</p>
         <div className="w-full h-96 flex flex-col gap-5 justify-center items-center">
           <Link to={`/cart/checkout`}>
             <p className="px-10 py-2 border text-xl bg-slate-800 text-white  rounded">
