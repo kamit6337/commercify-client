53d0ea53ec8188a767c0ffb508b3b38348acc87b
Kamit6337 <kamit6337@gmail.com>
2024-07-15 12:48:20 +0530

    change horizontal scrolling

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,129 +1,55 @@
 import { Link, Outlet } from "react-router-dom";
 import useAllCategory from "../hooks/query/useAllCategory";
-import { useEffect, useRef, useState } from "react";
-import { Icons } from "../assets/icons";
+import { useState } from "react";
+import HorizontalScrolling from "../lib/HorizontalScrolling";
 
 const HomeLayout = () => {
   const { data: allCategory } = useAllCategory();
-  const [scrollPixel, setScrollPixel] = useState(500);
-  const ref = useRef(null);
-  const [widthDiff, setWidthDiff] = useState(null);
-  const [index, setIndex] = useState(0);
   const [optionIndex, setOptionIndex] = useState(null);
-
-  useEffect(() => {
-    if (ref.current) {
-      const width = ref.current.clientWidth;
-      const scrollWidth = ref.current.scrollWidth;
-      setWidthDiff(scrollWidth - width);
-    }
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 700) {
-        setScrollPixel(300);
-      } else {
-        setScrollPixel(500);
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const moveRight = () => {
-    if (!widthDiff || widthDiff <= 0) return;
-    if (widthDiff >= scrollPixel) {
-      setIndex((prev) => prev - scrollPixel);
-      setWidthDiff((prev) => prev - scrollPixel);
-      return;
-    }
-
-    setIndex((prev) => prev - widthDiff);
-    setWidthDiff((prev) => prev - widthDiff);
-  };
-
-  const moveLeft = () => {
-    if (!index || index >= 0) return;
-
-    const positiveIndex = index * -1;
-
-    if (positiveIndex >= scrollPixel) {
-      setIndex((prev) => prev + scrollPixel);
-      setWidthDiff((prev) => prev + scrollPixel);
-      return;
-    }
-    setIndex((prev) => prev + positiveIndex);
-    setWidthDiff((prev) => prev + positiveIndex);
-  };
 
   return (
     <>
-      <div className="flex items-center h-32 relative">
-        <div className="relative flex-1 flex mx-20" ref={ref}>
-          <div
-            className="absolute h-full flex items-center gap-10 duration-700"
-            style={{ transform: `translateX(${index}px)` }}
-          >
-            {allCategory.data.length > 0 ? (
-              <>
-                <Link to={`/`}>
+      <HorizontalScrolling
+        height={125}
+        show_bg_color={true}
+        sideMargin={100}
+        inBetweenGap={50}
+      >
+        {allCategory.data.length > 0 ? (
+          <>
+            <Link to={`/`}>
+              <p
+                className={`${
+                  optionIndex === 999 && "border-b-2"
+                }  capitalize whitespace-nowrap text-category_text`}
+                onMouseEnter={() => setOptionIndex(999)}
+                onMouseLeave={() => setOptionIndex(null)}
+              >
+                All
+              </p>
+            </Link>
+            {allCategory.data.map((category, i) => {
+              const { _id, title } = category;
+
+              return (
+                <Link to={`/category/${_id}`} key={i}>
                   <p
                     className={`${
-                      optionIndex === 999 && "border-b-2"
+                      optionIndex === i && "border-b-2"
                     }  capitalize whitespace-nowrap text-category_text`}
-                    onMouseEnter={() => setOptionIndex(999)}
+                    onMouseEnter={() => setOptionIndex(i)}
                     onMouseLeave={() => setOptionIndex(null)}
                   >
-                    All
+                    {title}
                   </p>
                 </Link>
-                {allCategory.data.map((category, i) => {
-                  const { _id, title } = category;
-
-                  return (
-                    <Link to={`/category/${_id}`} key={i}>
-                      <p
-                        className={`${
-                          optionIndex === i && "border-b-2"
-                        }  capitalize whitespace-nowrap text-category_text`}
-                        onMouseEnter={() => setOptionIndex(i)}
-                        onMouseLeave={() => setOptionIndex(null)}
-                      >
-                        {title}
-                      </p>
-                    </Link>
-                  );
-                })}
-              </>
-            ) : (
-              <div>No Category available</div>
-            )}
-          </div>
-        </div>
-        <p
-          className={`
-          ${index >= 0 && "hidden"}
-          absolute h-full left-0  bg-category_arrow_div cursor-pointer flex items-center px-1 text-3xl`}
-          onClick={moveLeft}
-        >
-          <Icons.leftArrow />
-        </p>
-        <p
-          className={`
-          ${(!widthDiff || widthDiff <= 0) && "hidden"}
-          absolute h-full right-0 bg-category_arrow_div cursor-pointer  flex items-center px-1 text-3xl`}
-          onClick={moveRight}
-        >
-          <Icons.rightArrow />
-        </p>
-      </div>
+              );
+            })}
+          </>
+        ) : (
+          <div>No Category available</div>
+        )}
+      </HorizontalScrolling>
       <Outlet />
     </>
   );
--- src/lib/HorizontalScrolling.jsx
+++ src/lib/HorizontalScrolling.jsx
@@ -1,18 +1,36 @@
 /* eslint-disable react/prop-types */
-import { useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Icons } from "../assets/icons";
 
 const HorizontalScrolling = ({
   children,
-  cursorClassName,
   scrollLength = 500,
-  height = 400,
+  height,
   duration = 500,
+  show_bg_color = false,
+  sideMargin = 40,
+  inBetweenGap = 20,
 }) => {
-  const scrollPixel = scrollLength;
+  const [scrollPixel, setScrollPixel] = useState(scrollLength);
+
   const ref = useRef(null);
   const [widthDiff, setWidthDiff] = useState(null);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 700) {
+        setScrollPixel(300);
+      } else {
+        setScrollPixel(scrollLength);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useLayoutEffect(() => {
     if (ref.current) {
@@ -50,29 +68,50 @@ const HorizontalScrolling = ({
 
   return (
     <div
-      className="flex items-center my-10 relative"
+      className="flex items-center relative"
       style={{ height: `${height}px` }}
     >
-      <div className="relative flex-1 flex mx-10 tablet:mx-5" ref={ref}>
+      <div
+        className={`relative flex-1 flex ${sideMargin}`}
+        ref={ref}
+        style={{
+          marginLeft: `${sideMargin}px`,
+          marginRight: `${sideMargin}px`,
+        }}
+      >
         <div
-          className={`absolute h-full flex items-center gap-5 duration-${duration}`}
-          style={{ transform: `translateX(${index}px)` }}
+          className={`absolute h-full flex items-center duration-${duration}`}
+          style={{
+            transform: `translateX(${index}px)`,
+            gap: `${inBetweenGap}px`,
+          }}
         >
           {children}
         </div>
       </div>
-      <p
-        className={`absolute h-full left-0  hover:scale-125 duration-300 cursor-pointer  flex items-center px-1 tablet:px-0 text-slate-800 text-3xl ${cursorClassName}`}
+      <div
+        className={`
+          ${index >= 0 && "hidden"}
+          ${show_bg_color && "bg-category_arrow_div"}
+          absolute h-full left-0 cursor-pointer flex items-center px-1 text-3xl`}
         onClick={moveLeft}
       >
-        <Icons.leftArrow />
-      </p>
-      <p
-        className={`absolute h-full right-0  hover:scale-125 duration-300 cursor-pointer  flex items-center px-1 tablet:px-0 text-slate-800 text-3xl ${cursorClassName}`}
+        <p className="hover:scale-125 duration-300 h-full flex items-center">
+          <Icons.leftArrow />
+        </p>
+      </div>
+      <div
+        className={`
+          ${(!widthDiff || widthDiff <= 0) && "hidden"}
+          ${show_bg_color && "bg-category_arrow_div"}
+
+          absolute h-full right-0 cursor-pointer  flex items-center px-1 text-3xl`}
         onClick={moveRight}
       >
-        <Icons.rightArrow />
-      </p>
+        <p className="hover:scale-125 duration-300 h-full flex items-center">
+          <Icons.rightArrow />
+        </p>
+      </div>
     </div>
   );
 };
