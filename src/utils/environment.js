ccb6c7103b768ff26dd21e08365255b90744feff
Kamit6337 <kamit6337@gmail.com>
2024-07-16 10:49:39 +0530

    remove client url from env

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,6 +1,5 @@
 const environment = {
   SERVER_URL: import.meta.env.VITE_APP_SERVER_URL,
-  CLIENT_URL: import.meta.env.VITE_APP_CLIENT_URL,
   NODE_ENV: import.meta.env.VITE_APP_NODE_ENV,
   STRIPE_PUBLISHABLE_KEY: import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY,
   CURRENCY_EXCHANGE_KEY: import.meta.env.VITE_APP_CURRENCY_EXCHANGE_KEY,
